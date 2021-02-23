1204a7e0cb79fcfa2ae1e06392fcb29b597d2c6f
zablon7 <zablonshewangizaw@gmail.com>
2021-02-23 23:12:11 +0300

    Updated.

--- src/Demo.java
+++ src/Demo.java
@@ -1,19 +1,28 @@
 import textio.TextIO;
 
 public class Demo{
-    static int x = 3;
+    int x = 3;
     public static void main(String[] args){
         Bruh yo = new Bruh();
         System.out.print("Enter number to be printed: ");
         System.out.println(yo.getIt());
 
     }
-
+    public interface ConversionFactors{
+        int INCHES_PER_FOOT = 12;
+        int FEET_PER_YARD = 3;
+        int YARDS_PER_MILE = 1760;
+        public void tryOut();
+    }
 }
 
-class Bruh extends Demo{
+class Bruh extends Demo implements Demo.ConversionFactors{
     public int getIt(){
         super.x = TextIO.getlnInt();
         return super.x;
     }
+    public void tryOut(){
+        System.out.println(INCHES_PER_FOOT);
+    }
 }
+
