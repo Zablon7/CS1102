1b82f53d3b8f57f1156781678df86fe3b71b9b4d
zablon7 <zablonshewangizaw@gmail.com>
2021-02-24 01:56:24 +0300

    Updated

--- src/Demo.java
+++ src/Demo.java
@@ -1,16 +1,13 @@
-import javafx.scene.canvas.GraphicsContext;
-
 public class Demo{
-
     public static void main(String[] args) {
-        Strokeable figure;
-        figure = new Circle();
-        GraphicsContext x = null;
-        figure.stroke(x);
+        Demo.line bruh = new Demo.line();
+        bruh.x1 = 3;
+        System.out.println(bruh.x1);
+    }
 
-        Strokeable[] listOfFigures;
-        listOfFigures = new Strokeable[10];
-        listOfFigures[0] = new Circle();
+    public static class line{
+        double x1, y1, z1;
+        double x2, y2, z2;
 
     }
 }
