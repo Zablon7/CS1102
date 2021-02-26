7d42635f8b2d3a006ba50b7b0802cb34123a5a29
zablon7 <zablonshewangizaw@gmail.com>
2021-02-26 05:09:27 +0300

    Updated

--- src/Banks.java
+++ src/Banks.java
@@ -1,52 +1,74 @@
 import textio.TextIO;
 
 class Banks{
-    protected int AccNum;
-    private int amount;
-    int remainingNum = 10000;
+
+    /* This program is a model of a small bank and two different services they give;
+    The first is ATM, where they can withdraw their money;
+    For keeping this example simple, there is no restriction on the length of the account number;
+    The second service is Mobile Banking, where they can send or receive money;
+    * We will use "this" and "super" to access different instance variables and methods in this program;
+    As the methodology is repeated throughout the program;
+    Comments are found on different parts of the program;
+    */
+
+    protected int AccNum; // Protected means subclasses can access this variable.
+    private int amount; // Amount of money in bank account;
+    int remainingNum = 10000; // Our starting amount for any account
     int withdrawAmount;
 
 
-    int Balance(){
-        amount = remainingNum-withdrawAmount;
+    int Balance(){  // Method to keep track of balance of the account
+        amount = remainingNum-withdrawAmount; // We use "this" to access the amount variable
        return amount;
     }
 
     public static void main(String[] args){
         ATM atm = new ATM();
-        atm.withdraw(atm.AccNum);
+
     }
 }
 
 class ATM extends Banks{
     private int PIN;
+    ATM(){
+        this.withdraw();
+        System.out.println("Your balance is $" + super.Balance());
+    }
 
-    void withdraw(int AccountNumber){
+    void withdraw(){
         System.out.print("Enter Account Number: ");
-        AccountNumber=TextIO.getlnInt();
-        super.AccNum = AccountNumber;
+        super.AccNum=TextIO.getlnInt();  // We use "super" to access the AccNum from the main Bank class
         System.out.print("Enter PIN: ");
-        this.PIN = TextIO.getlnInt();
-        while (Integer.toString(this.PIN).length() != 4){
+        this.PIN = TextIO.getlnInt(); // We use "this" to access the PIN variable from this, ATM class
+        while (Integer.toString(this.PIN).length() != 4){ // Forces the pin number of the ATM card to be 4 digits
             System.out.println("Invalid. Try Again.");
             this.PIN = TextIO.getlnInt();
         }
         System.out.print("Enter amount you want to withdraw: ");
-        super.withdrawAmount=TextIO.getlnInt();
+        super.withdrawAmount=TextIO.getlnInt(); // We use super to access
+
+        //This while loop forces the withdraw amount to be valid and within the sufficient balance
         while(super.withdrawAmount > 10000 || super.withdrawAmount < 1){
             System.out.println("Invalid operation, enter an amount between 1 and 10000");
             System.out.print("Amount: ");
-            super.withdrawAmount = TextIO.getlnInt();
+            super.withdrawAmount = TextIO.getlnInt(); //Using "super" to access withdrawAmount variable from the superclass
         }
         System.out.println("Successful.");
-        System.out.println("Your remaining amount is $" + super.Balance());
+         // Using "super" to access the withdrawAmount variable from the superclass
+        System.out.println("Your have withdrawn $" + super.withdrawAmount);
     }
 }
 
 class MobileBanking extends Banks{
     protected int receiver;
     protected int sender;
-    private int amount;
+    private int amount; // We make this amount variable to be private so it won't cause a bug by acting as the amount in the superclass
+
+    MobileBanking(){
+        this.send(); // we use "this" to access the send method in this class
+        // We use "super" to access the Balance method in the superclass
+        System.out.println("Your balance is $" + super.Balance());
+    }
 
     void send(){
         System.out.print("Enter receiver's account number: ");
@@ -59,12 +81,12 @@ class MobileBanking extends Banks{
             super.withdrawAmount = TextIO.getlnInt();
         }
         System.out.println("Sent.");
-        System.out.println("Your remaining amount is $" + super.Balance());
+        System.out.println("Your have sent  $" + super.withdrawAmount); // Using "super" to call the "withdrawAmount" variable from the superclass.
     }
 
     void receive(){
         System.out.print("Enter sender's account number: ");
-        this.sender = TextIO.getlnInt();
+        this.sender = TextIO.getlnInt(); // Using "this" to access the sender variable from this, MobileBanking class.
         System.out.print("Enter amount you want to request: ");
         this.amount = TextIO.getlnInt();
         System.out.println("Request for $" + this.amount + " sent");
