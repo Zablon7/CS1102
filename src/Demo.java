e7630e2e96a6f991df39aded49eaaae5bcd5b84b
zablon7 <zablonshewangizaw@gmail.com>
2021-03-13 14:16:22 +0300

    Updated

--- src/Demo.java
+++ src/Demo.java
@@ -1,12 +1,38 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class Demo extends Application {
-    public static void main(String[] args){
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 
+public class Demo extends JFrame{
+
+    Demo(){
+        ImageIcon image = new ImageIcon("src/icon.jpg");
+
+        Border border = BorderFactory.createLineBorder(Color.GREEN, 3);
+
+        JLabel label = new JLabel();
+        label.setText("Bro, do you even code?");
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(Color.RED);
+        label.setFont(new Font("MV Boli", Font.ITALIC, 20));
+        label.setIconTextGap(-10);
+        label.setBackground(Color.BLACK);
+        label.setOpaque(true);
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBounds(100, 0, 250, 250); //set x,y position within frame as well as dimensions
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setLayout(null);
+        this.setVisible(true);
+        this.add(label);
     }
-    public void start(Stage stage){
+    public static void main(String[] args){
+        new Demo();
 
     }
 }
